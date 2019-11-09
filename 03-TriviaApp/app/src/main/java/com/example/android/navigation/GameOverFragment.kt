98f3db8e266fdab0d8cbc8f5c453a42023d34d39
Anderson Mendes <anderson2000ano@gmail.com>
2019-11-09 18:25:28 -0300

    03-TriviaApp: Back Stack Manipulation

--- 03-TriviaApp/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ 03-TriviaApp/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,7 +22,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
+import kotlinx.android.synthetic.main.fragment_game_over.*
 
 class GameOverFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +33,11 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
+
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
+        }
+
         return binding.root
     }
 }
